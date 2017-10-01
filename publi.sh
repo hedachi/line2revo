a285ca47255c3324753ee207c984c300d3a3e4f1
Takayuki Fukuda <fukuda_82@yahoo.co.jp>
2017-10-02 08:33:38 +0900

    commit via publi.sh

--- publi.sh
+++ publi.sh
@@ -1,5 +1,6 @@
 slimrb -p *.slim *.html
 git add --all
 git commit -m 'commit via publi.sh'
+git push origin HEAD
 echo '-------------------------------- finished --------------------------------'
 git status
